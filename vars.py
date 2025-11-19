45edc4b370acf0e283e253dc09a7922e852aabab
rcbcc25-sketch <rcbcc25@gmail.com>
2025-11-19 23:33:27 +0530

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,9 @@
+#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+import os
+
+API_ID    = os.environ.get("API_ID", "29731917")
+API_HASH  = os.environ.get("API_HASH", "d0b73a75f2d12cae5b04c21044ff0148")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8440417637:AAGL8Tof164pZrxFbwaIWVFZlq2FUTufs6Y")
+#WEBHOOK = True  # Don't change this
+#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
